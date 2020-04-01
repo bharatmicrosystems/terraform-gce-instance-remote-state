bfc8d953d0bed8db352130751a3f185123d83c5f
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-01 22:30:48 +0100

    Updated provider

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("/var/lib/jenkins/credentials.json")}"
- project     = "${var.project}"
- region      = "${var.region}"
+ credentials = ${file("/var/lib/jenkins/credentials.json")}
+ project     = ${var.project}
+ region      = ${var.region}
 }
 
 variable "project" {}
