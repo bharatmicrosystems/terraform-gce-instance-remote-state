ba9949cfd406bdc902ace455ec9e6f4e5a764a82
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-01 21:58:35 +0100

    Hardcoded credentials

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file(var.credentials)}"
+ credentials = "${file("/var/lib/jenkins/credentials.json")}"
  project     = "${var.project}"
  region      = "${var.region}"
  version     = "1.19.0"
